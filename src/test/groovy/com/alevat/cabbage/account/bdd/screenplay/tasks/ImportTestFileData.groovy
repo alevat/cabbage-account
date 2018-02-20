208780438a34cb80734c015e8fd9ff3d43561dc1
Eric Tavela <etavela@5amsolutions.com>
2018-02-20 08:59:17 -0500

    Fixed file location

--- src/test/groovy/com/alevat/cabbage/account/bdd/screenplay/tasks/ImportTestFileData.groovy
+++ src/test/groovy/com/alevat/cabbage/account/bdd/screenplay/tasks/ImportTestFileData.groovy
@@ -4,14 +4,15 @@ import com.alevat.cabbage.account.domain.ImportFormat
 
 class ImportTestFileData {
 
-    private final ImportFormat format
-    private final File file
+    private ImportFormat format
+    private File file
 
     static ImportTestFileData get(String typeName) {
         def formatName = typeName.toUpperCase().replace(' ', '_')
         def format = ImportFormat.valueOf(formatName)
         def filename = format.name().toLowerCase() + "_import_file.csv"
-        def file = ImportTestFileData.class.getResource("data/${filename}").getFile()
+        def filePath = ImportTestFileData.class.getResource("/data/${filename}").getFile()
+        def file = new File(filePath)
         return new ImportTestFileData(format: format, file: file)
     }
 
